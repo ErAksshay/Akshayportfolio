a3f5f0a63ae705c15ecc471f6f2362c728c5257f
Akshaya Kumar <75885567+Akshaya-Developer@users.noreply.github.com>
2023-12-25 22:40:34 +0530

    Git init

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'akshay801998@gmail.com',
+    phone: '+917018733597',
+    address: 'House No. 35, Village Bajroh PO Badhani Teh Bhoranj Distt Hamirpur H.P., India - 17601 ',
+
+    sheetAPI: ''
+}
